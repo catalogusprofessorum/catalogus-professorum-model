eae76b8d6e4fd889be74feb2d0f1ef854e9cf5ee
triechert <thomas.riechert@htwk-leipzig.de>
2018-05-04 14:12:24 +0200

    more modular version of the generating script

---
+++ tools/widoco/generate.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# ONTO contains version (First Argument)
+ONTO="$1"
+echo "Generating Documentation for /cpm/$ONTO"
+cd /var/www
+
+#update ontology
+wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-$ONTO/ontology.ttl -O ontology-cpm-$ONTO.ttl
+
+#generate documentation for Version 1.9
+java -jar widoco-1.4.3-jar-with-dependencies.jar -ontFile ontology-cpm-1.ttl -lang en-de -webVowl -rewriteAll -includeAnnotationProperties -outFolder html/cpm/$ONTO/
+
+#warte 20 Sekunden
+echo "Documentation generated. Wait now 20s to copy additional files."  
+sleep 20
+
+#start post processing
+
+#extend the generated documentation
+wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/description-en.html -O html/cpm/$ONTO/sections/description-en.html
+wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/description-de.html -O html/cpm/$ONTO/sections/description-de.html
+wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/references.html -O html/cpm/$ONTO/sections/references.html
+
+#add introduction
+wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/intro-en.html -O html/cpm/$ONTO/intro-en.html
+more +6 html/cpm/$ONTO/introduction-en.html >> html/cpm/$ONTO/intro-en.html
+mv html/cpm/$ONTO/intro-en.html html/cpm/$ONTO/introduction-en.html
+wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/intro-de.html -O html/cpm/$ONTO/intro-de.html
+more +6 html/cpm/$ONTO/introduction-de.html >> html/cpm/$ONTO/intro-de.html
+mv html/cpm/$ONTO/intro-de.html html/cpm/$ONTO/introduction-de.html
+
+#German translations
+sed s/Cross\ reference\ for/Übersicht\ über/g -i ./html/cpm/$ONTO/sections/crossref-de.html
+sed s/This\ section\ provides\ details\ for\ each\ class\ and\ property\ defined\ by/Dieser\ Abschnitt\ beschreibt\ detailliert\ die\ Klassen\ und\ Rollen\,\ die\ definiert\ sind\ durch/g -i ./html/cpm/$ONTO/sections/crossref-de.html
+sed s/Table\ of\ contents/Inhaltsverzeichnis/g -i ./html/cpm/$ONTO/index-de.html
+sed s/Download\ serialization/Download\ in\ verschiedenen\ Datenformaten/g -i ./html/cpm/$ONTO/index-de.html
+sed s/License\:/Datenlizenz\:/g -i ./html/cpm/$ONTO/index-de.html
+sed s/Visualization/Darstellung\ als\ Graph/g -i ./html/cpm/$ONTO/index-de.html
+sed s/Acknowledgements/Danksagung/g -i ./html/cpm/$ONTO/index-de.html
+sed s/References/Referenzen/g -i ./html/cpm/1/sections/references-de.html
+
+#Authors, Maintainer and other Version Section
+wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/authors.html -O html/cpm/$ONTO/sections/authors-en.html
+cp html/cpm/$ONTO/sections/authors-en.html html/cpm/$ONTO/sections/authors-de.html
+sed s/Authors/Autoren/g -i ./html/cpm/$ONTO/sections/authors-de.html
+sed s/Other\ Versions/Weitere\ Versionen/g -i ./html/cpm/$ONTO/sections/authors-de.html
+sed s/\<\\/dl\>/\<\\/dl\>\<dl\ id\=\"authors\"\>\<\\/dl\>\<p\\/\>/g -i ./html/cpm/$ONTO/index-en.html 
+sed s/\$\(\"\#abstract\"\)\.load/\$\(\"\#authors\"\)\.load\(\"sections\\/authors\-en.html\"\)\;\$\(\"\#abstract\"\)\.load/g -i ./html/cpm/$ONTO/index-en.html 
+sed s/\<\\/dl\>/\<\\/dl\>\<dl\ id\=\"authors\"\>\<\\/dl\>\<p\\/\>/g -i ./html/cpm/$ONTO/index-de.html 
+sed s/\$\(\"\#abstract\"\)\.load/\$\(\"\#authors\"\)\.load\(\"sections\\/authors\-de.html\"\)\;\$\(\"\#abstract\"\)\.load/g -i ./html/cpm/$ONTO/index-de.html 
+
+
+# Content Negoiation for rdf formats
+echo "<?php header('HTTP/1.1 303 See Other');"  	> /html/cpm/$ONTO/index.php
+echo "$accept = explode(',', $_SERVER['HTTP_ACCEPT']);"	>> /html/cpm/$ONTO/index.php
+echo "if (in_array('text/turtle ', $accept))  header('Location: ontology.ttl');" >> /html/cpm/$ONTO/index.php
+echo "else if (in_array('application/n-triples', $accept)) header('Location: ontology.nt');" >> /html/cpm/$ONTO/index.php
+echo "elseif (in_array('application/ld+json', $accept)) header('Location: ontology.json');" >> /html/cpm/$ONTO/index.php
+echo "elseif (in_array('application/rdf+xml', $accept)) header('Location: ontology.xml');" >> /html/cpm/$ONTO/index.php
+echo "else header('Location: index-en.html');" >> /html/cpm/$ONTO/index.php
+echo "?>" >> /html/cpm/$ONTO/index.php
+
--- tools/widoco/startup.sh
+++ tools/widoco/startup.sh
@@ -1,121 +1,26 @@
 #!/bin/bash
 
+# provide the web path for the URIs
 cd /var/www
 mkdir /var/www/html/cpm/
 mkdir /var/www/html/cpm/1
 mkdir /var/www/html/cpm/2
 
+# download WIDOC tool if not exists
 if [ ! -f widoco-1.4.3-jar-with-dependencies.jar ]; then
     wget https://github.com/dgarijo/Widoco/releases/download/v1.4.3/widoco-1.4.3-jar-with-dependencies.jar
 fi
 
-### Version 1.9
+#update generate.sh
+wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/tools/widoco/generate.sh -O generate.sh
+chmod a+rx generate.sh
 
-mkdir cpm-1
-cd cpm-1
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/ontology.ttl -O ontology.ttl
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/description-en.html -O description-en.html
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/description-de.html -O description-de.html
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/introduction-en.html -O introduction-en.html
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/introduction-de.html -O introduction-de.html
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/references.html -O references.html
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-1/authors.html -O authors.html
+./generate.sh 1
+-/generate.sh 2
 
-cd /var/www
-java -jar widoco-1.4.3-jar-with-dependencies.jar -ontFile cpm-1/ontology.ttl -lang en-de -webVowl -rewriteAll -includeAnnotationProperties -outFolder html/cpm/1/
+# Content Negoiation /cpm/
+echo "<?php header('HTTP/1.1 303 See Other');"  	> /html/cpm/$index.php
+echo "header('Location: /cpm/2/');" >> /html/cpm/$index.php
+echo "?>" >> /html/cpm/$index.php
 
-sleep 30
-
-#Post Processing
-
-#German Translations
-sed s/Cross\ reference\ for/Übersicht\ über/g -i ./html/cpm/1/sections/crossref-de.html
-sed s/This\ section\ provides\ details\ for\ each\ class\ and\ property\ defined\ by/Dieser\ Abschnitt\ beschreibt\ detailliert\ die\ Klassen\ und\ Rollen\,\ die\ definiert\ sind\ durch/g -i ./html/cpm/1/sections/crossref-de.html
-sed s/Table\ of\ contents/Inhaltsverzeichnis/g -i ./html/cpm/1/index-de.html
-sed s/Download\ serialization/Download\ in\ verschiedenen\ Datenformaten/g -i ./html/cpm/1/index-de.html
-sed s/License\:/Datenlizenz\:/g -i ./html/cpm/1/index-de.html
-sed s/Visualization/Darstellung\ als\ Graph/g -i ./html/cpm/1/index-de.html
-sed s/Acknowledgements/Danksagung/g -i ./html/cpm/1/index-de.html
-
-#Add latest version
-sed s/\<dd\>Version/\<dd\>\<a\ href\=\"http\:\\/\\/catalogus-professorum.org\\/cpm\\/2\\/\"\>Version\ 2\.1\ \-\ 2018\-04\-26\ \-\ Thomas\ Riechert\ \(Latest\)\<\\/a\>\<\\/dd\>\<dd\>Version/g -i ./html/cpm/1/index-en.html
-sed s/\<dd\>Version/\<dd\>\<a\ href\=\"http\:\\/\\/catalogus-professorum.org\\/cpm\\/2\\/\"\>Version\ 2\.1\ \-\ 2018\-04\-26\ \-\ Thomas\ Riechert\ \(Aktuelle\ Version\)\<\\/a\>\<\\/dd\>\<dd\>Version/g -i ./html/cpm/1/index-de.html
-
-
-#Description and Introduction Sections
-cp cpm-1/description-en.html ./html/cpm/1/sections/description-en.html
-cp cpm-1/description-de.html ./html/cpm/1/sections/description-de.html
-cp cpm-1/introduction-en.html ./html/cpm/1/sections/introduction-en.html
-cp cpm-1/introduction-de.html ./html/cpm/1/sections/introduction-de.html
-
-#References Section
-cp cpm-1/references.html ./html/cpm/1/sections/references-en.html
-cp cpm-1/references.html ./html/cpm/1/sections/references-de.html
-sed s/References/Referenzen/g -i ./html/cpm/1/sections/references-de.html
-
-#Authors and Maintainer Section
-cp cpm-1/authors.html ./html/cpm/1/sections/authors-en.html
-cp cpm-1/authors.html ./html/cpm/1/sections/authors-de.html
-sed s/Authors/Autoren/g -i ./html/cpm/1/sections/authors-de.html
-sed s/Cross\ reference\ for/Übersicht\ über/g -i ./html/cpm/1/sections/crossref-de.html
-sed s/\<\\/dl\>/\<\\/dl\>\<dl\ id\=\"authors\"\>\<\\/dl\>\<p\\/\>/g -i ./html/cpm/1/index-en.html 
-sed s/\$\(\"\#abstract\"\)\.load/\$\(\"\#authors\"\)\.load\(\"sections\\/authors\-en.html\"\)\;\$\(\"\#abstract\"\)\.load/g -i ./html/cpm/1/index-en.html 
-sed s/\<\\/dl\>/\<\\/dl\>\<dl\ id\=\"authors\"\>\<\\/dl\>\<p\\/\>/g -i ./html/cpm/1/index-de.html 
-sed s/\$\(\"\#abstract\"\)\.load/\$\(\"\#authors\"\)\.load\(\"sections\\/authors\-de.html\"\)\;\$\(\"\#abstract\"\)\.load/g -i ./html/cpm/1/index-de.html 
-
-# Content Negoiation
-echo '<?php header("HTTP/1.1 303 See Other");header("Location: http://$_SERVER['HTTP_HOST']/cpm/1/index-en.html"); ?>' > ./html/cpm/1/index.php
-
-
-### Version 2.1
-mkdir cpm-2
-cd cpm-2
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-2/ontology.ttl -O ontology.ttl
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-2/description-en.html -O description-en.html
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-2/description-de.html -O description-de.html
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-2/introduction-en.html -O introduction-en.html
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-2/introduction-de.html -O introduction-de.html
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-2/references.html -O references.html
-wget https://raw.githubusercontent.com/pcp-on-web/catalogus-professorum-lipsiensium/master/model/cpm-2/authors.html -O authors.html
-
-cd /var/www
-java -jar widoco-1.4.3-jar-with-dependencies.jar -ontFile cpm-2/ontology.ttl -lang en-de -webVowl -rewriteAll -includeAnnotationProperties -outFolder html/cpm/2/
-sleep 30
-
-#Post Processing
-
-#German Translations
-sed s/Cross\ reference\ for/Übersicht\ über/g -i ./html/cpm/2/sections/crossref-de.html
-sed s/This\ section\ provides\ details\ for\ each\ class\ and\ property\ defined\ by/Dieser\ Abschnitt\ beschreibt\ detailliert\ die\ Klassen\ und\ Rollen\,\ die\ definiert\ sind\ durch/g -i ./html/cpm/2/sections/crossref-de.html
-sed s/Table\ of\ contents/Inhaltsverzeichnis/g -i ./html/cpm/2/index-de.html
-sed s/Download\ serialization/Download\ in\ verschiedenen\ Datenformaten/g -i ./html/cpm/2/index-de.html
-sed s/License\:/Datenlizenz\:/g -i ./html/cpm/2/index-de.html
-sed s/Visualization/Darstellung\ als\ Graph/g -i ./html/cpm/2/index-de.html
-sed s/Acknowledgements/Danksagung/g -i ./html/cpm/2/index-de.html
-
-#Description and Introduction Sections
-cp cpm-2/description-en.html ./html/cpm/2/sections/description-en.html
-cp cpm-2/description-de.html ./html/cpm/2/sections/description-de.html
-cp cpm-2/introduction-en.html ./html/cpm/2/sections/introduction-en.html
-cp cpm-2/introduction-de.html ./html/cpm/2/sections/introduction-de.html
-
-#References Section
-cp cpm-2/references.html ./html/cpm/2/sections/references-en.html
-cp cpm-2/references.html ./html/cpm/2/sections/references-de.html
-sed s/References/Referenzen/g -i ./html/cpm/2/sections/references-de.html
-
-#Authors and Maintainer Section
-cp cpm-2/authors.html ./html/cpm/2/sections/authors-en.html
-cp cpm-2/authors.html ./html/cpm/2/sections/authors-de.html
-sed s/Authors/Autoren/g -i ./html/cpm/2/sections/authors-de.html
-sed s/Cross\ reference\ for/Übersicht\ über/g -i ./html/cpm/2/sections/crossref-de.html
-sed s/\<\\/dl\>/\<\\/dl\>\<dl\ id\=\"authors\"\>\<\\/dl\>\<p\\/\>/g -i ./html/cpm/2/index-en.html 
-sed s/\$\(\"\#abstract\"\)\.load/\$\(\"\#authors\"\)\.load\(\"sections\\/authors\-en.html\"\)\;\$\(\"\#abstract\"\)\.load/g -i ./html/cpm/2/index-en.html 
-sed s/\<\\/dl\>/\<\\/dl\>\<dl\ id\=\"authors\"\>\<\\/dl\>\<p\\/\>/g -i ./html/cpm/2/index-de.html 
-sed s/\$\(\"\#abstract\"\)\.load/\$\(\"\#authors\"\)\.load\(\"sections\\/authors\-de.html\"\)\;\$\(\"\#abstract\"\)\.load/g -i ./html/cpm/2/index-de.html 
-
-# Content Negoiation
-echo '<?php header("HTTP/1.1 303 See Other");header("Location: http://$_SERVER['HTTP_HOST']/cpm/2/index-en.html"); ?>' > ./html/index.php
-echo '<?php header("HTTP/1.1 303 See Other");header("Location: http://$_SERVER['HTTP_HOST']/cpm/2/index-en.html"); ?>' > ./html/cpm/index.php
-echo '<?php header("HTTP/1.1 303 See Other");header("Location: http://$_SERVER['HTTP_HOST']/cpm/2/index-en.html"); ?>' > ./html/cpm/2/index.php
 
